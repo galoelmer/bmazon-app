06d4e969e60f754dc137f005b4c8bd64a7574beb
Elmer C. Galo <elmer.galo@outlook.com>
2019-11-28 22:21:05 -0500

    Add functionality to place an order and update database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
-var AsciiTable = require('ascii-table');
+var inquirer = require("inquirer");
+var AsciiTable = require("ascii-table");
 
 // Create the connection information for the sql database
 var connection = mysql.createConnection({
@@ -24,14 +25,9 @@ connection.connect(function (err) {
     connection.query("SELECT * FROM products", function (err, results) {
         if (err) throw err;
 
-        // Create a list of arrays with items's ids, names and prices from query result
+        // Create a list of arrays with item's ids, names and prices from query result
         let tableItems = results.map(item => {
-            return
-            [
-                item.item_id,
-                item.product_name,
-                `$${item.price.toFixed(2)}`
-            ];
+            return [item.item_id, item.product_name, `$${item.price.toFixed(2)}`];
         });
 
         // Table instant creator
@@ -41,7 +37,60 @@ connection.connect(function (err) {
             , rows: tableItems
         });
 
-        console.log(table.toString())
+        console.log(table.toString());
+        start();
     });
 });
 
+function start() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "id",
+                message: "Enter Product ID you would like to buy"
+            },
+            {
+                type: "number",
+                name: "quantity",
+                message: "How many units would you like to buy?"
+            }
+        ]).then(function (customerRequest) {
+            connection.query(
+                "SELECT stock_quantity, price FROM products WHERE item_id=?", [customerRequest.id], function (err, product) {
+                if (err) throw err;
+
+                if (customerRequest.quantity <= product[0].stock_quantity) {
+                    placeOrder(customerRequest, product);
+                } else {
+                    console.log("\n***** Insufficient Quantity *****\n");
+                }
+            });
+        });
+}
+
+function placeOrder(customerRequest, product) {
+    connection.query(
+        "UPDATE products SET ? WHERE ?",
+        [
+            {
+                stock_quantity: product[0].stock_quantity - customerRequest.quantity
+            },
+            {
+                item_id: customerRequest.id
+            }
+        ],
+        function (error) {
+            if (error) throw err;
+            var totalCost = product[0].price * customerRequest.quantity;
+
+            var table = new AsciiTable();
+            table
+                .setHeading('Order Total')
+                .addRow(totalCost.toLocaleString('en-IN', { style: 'currency', currency: 'USD' }))
+                .setAlign(0, AsciiTable.CENTER);
+
+            console.log(table.toString());
+        }
+    );
+}
